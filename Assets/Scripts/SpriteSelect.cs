84bdfa48b77df30652380086cdd4f42cf8d7f1de
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-07-20 22:58:18 +0200

    Better scrolling; Scroll added to sprite select

--- Assets/Scripts/SpriteSelect.cs
+++ Assets/Scripts/SpriteSelect.cs
@@ -12,6 +12,7 @@ public class SpriteSelect : SingletonMonoBehaviour<SpriteSelect>
 	public Button FinishSelectingButton;
 	public Text Title;
 	public GameObject SpriteObjectPrefab;
+	public GameObject Sprites;
 
 	public bool IsOpened => gameObject.transform.parent.gameObject.activeSelf;
 
@@ -19,7 +20,7 @@ public class SpriteSelect : SingletonMonoBehaviour<SpriteSelect>
 	{
 		gameObject.transform.parent.gameObject.Show();
 		SelectedObjects.Clear();
-		gameObject.transform.Find("Sprites").transform.Clear(); //Careful! Removes probably on the next frame
+		Sprites.transform.Clear(); //Careful! Removes probably on the next frame
 		_objectsToFill = new List<NKMObject>(objectsToFill);
 		_objectsToFill.ForEach(SpawnSpriteObject);
 		FinishSelectingButton.onClick.RemoveAllListeners();
@@ -29,12 +30,12 @@ public class SpriteSelect : SingletonMonoBehaviour<SpriteSelect>
 		FinishSelectingButton.GetComponentInChildren<Text>().text = finishButtonText;
 
 		//select item if is the only one
-		Transform spritesTransform = gameObject.transform.Find("Sprites").transform;
+		Transform spritesTransform = Sprites.transform;
 		if (_objectsToFill.Count == 1) spritesTransform.GetComponentsInChildren<Button>()[spritesTransform.childCount-1].onClick.Invoke(); //get last button, because the others are not removed yet for some reason
 	}
 	private void SpawnSpriteObject(NKMObject o)
 	{
-		GameObject spriteObject = Instantiate(SpriteObjectPrefab, gameObject.transform.Find("Sprites").transform);
+		GameObject spriteObject = Instantiate(SpriteObjectPrefab, Sprites.transform);
 		var button = spriteObject.GetComponent<Button>();
 		button.onClick.AddListener(delegate
 		{
