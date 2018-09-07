8269541a997919ce22d8d218f313f45c6062a8e8
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-09-07 22:19:40 +0200

    Shortcut Manager added; Server List scene and useful prefabs added

---
+++ Assets/Scripts/Managers/ServerList.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers
+{
+	public class ServerList : MonoBehaviour
+	{
+		public InputField AddServerName;
+		public InputField AddServerIP;
+		public Button AddServerButton;
+		private void Awake()
+		{
+			AddServerButton.onClick.AddListener(()=> {
+				SaveServerInfo();
+				RefreshList();
+			});
+		}
+
+		private void RefreshList()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		private void SaveServerInfo()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/ShortcutManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Managers
+{
+	public class ShortcutManager : CreatableSingletonMonoBehaviour<ShortcutManager>
+	{
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void DDOL() => DontDestroyOnLoad(Instance);
+
+		private void Update()
+		{
+			if (new[] {Scenes.MainMenu, Scenes.PreGameOptions}.Contains(SceneManager.GetActiveScene().name))
+			{
+				if (Input.GetKeyDown(KeyCode.LeftArrow)) LoadLastScene();
+				if (Input.GetKeyDown(KeyCode.RightArrow)) ClickActiveButton();
+			}
+		}
+		
+		private readonly Stack<string> _lastScenes = new Stack<string>();
+		private void Awake() => SceneManager.sceneLoaded += (scene, mode) => _lastScenes.Push(scene.name);
+
+		private void LoadLastScene()
+		{
+			if(_lastScenes.Count <= 1) return;
+			_lastScenes.Pop(); //Remove current scene name
+			SceneManager.LoadScene(_lastScenes.Pop()); //Remove last scene name and load it
+		}
+
+		private static void ClickActiveButton()
+		{
+			GameObject g = EventSystem.current.currentSelectedGameObject;
+            if(g!=null) g.GetComponent<Button>()?.onClick.Invoke();
+		}
+	}
+}
