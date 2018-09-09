b07047e4a9f35bee2cc799bdc8abb96d97f39a74
Krzysztof Ruczkowski <tojatos@gmail.com>
2018-09-09 07:09:31 +0200

    Background camera now creates itself before scene load

---
+++ Assets/Scripts/CreatableBackgroundCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CreatableBackgroundCamera : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void DDOL() => DontDestroyOnLoad(ThisObject);
+
+	private static CreatableBackgroundCamera ThisObject => 
+		Instantiate(Stuff.Prefabs.Find(p => p.name == "Background Camera"))
+			.AddComponent<CreatableBackgroundCamera>()
+			.GetComponent<CreatableBackgroundCamera>();
+}
