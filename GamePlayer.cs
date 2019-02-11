cd1d51edc2a9ffeace45e0ad9505ac8e8720dc34
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-02-11 23:50:41 +0100

    refactor of adding characters to gameplayer

--- GamePlayer.cs
+++ GamePlayer.cs
@@ -19,26 +19,13 @@ namespace NKMCore
 
 		public void AddCharacter(Character character)
 		{
-//		character.Owner = this;
 			Characters.Add(character);
 		}
 
-//	public void AddCharacter(string characterName) => AddCharacter(new Character(characterName));
 		public void AddCharacter(Game game, string characterName) => AddCharacter(CharacterFactory.Create(game, characterName));
-
-		private void AddCharacters(List<Character> characters)
+		public void AddCharacters(string[] characterNames)
 		{
-//		characters.ForEach(c=> c.Owner = this);
-			Characters.AddRange(characters);
-		}
-
-		public void AddCharacters(IEnumerable<string> characterNames)
-		{
-			List<Character> characters = new List<Character>();
-//		characterNames.ToList().ForEach(n => characters.Add(new Character(n)));
-			characterNames.ToList().ForEach(n => characters.Add(CharacterFactory.Create(Game, n)));
-			AddCharacters(characters);
-
+			Characters.AddRange(characterNames.Select(c => CharacterFactory.Create(Game, c)));
 		}
 	}
 }
