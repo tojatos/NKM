55e470c88c64c84a8fc1e8e6013f10cd590546a8
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-02-15 01:06:33 +0100

    Conflagration decouple; HexCellEffect refactor

--- Hex/HexMap.cs
+++ Hex/HexMap.cs
@@ -31,6 +31,10 @@ namespace NKMCore.Hex
 		    
 	    public Delegates.CharacterCell AfterMove;
 	    public Delegates.CharacterCell AfterCharacterPlace;
+	    public Delegates.HexCellEffectD AfterCellEffectCreate;
+	    public Delegates.HexCellEffectD AfterCellEffectRemove;
+	    public void InvokeAfterCellEffectCreate(HexCellEffect e) => AfterCellEffectCreate?.Invoke(e);
+	    public void InvokeAfterCellEffectRemove(HexCellEffect e) => AfterCellEffectRemove?.Invoke(e);
 
 	    /// <summary>
 	    /// Removes character from map or does nothing
