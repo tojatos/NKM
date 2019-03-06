527648336e57d112c1ad7ceecf215aaaaef94087
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-03-06 23:07:30 +0100

    Windows build fixes

--- Assets/Scripts/Unity/Managers/Client.cs
+++ Assets/Scripts/Unity/Managers/Client.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using NKMCore;
-using UnityEditor;
-using UnityEngine;
-using Action = System.Action;
 
 namespace Unity.Managers
 {
