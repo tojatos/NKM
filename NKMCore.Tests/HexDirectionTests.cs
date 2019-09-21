e44f0225e60bfa1fbf7bd1918c2772f576197de5
tojatos <tojatos@gmail.com>
2019-09-21 13:56:04 +0200

    NKMCore.Tests added

---
+++ NKMCore.Tests/HexDirectionTests.cs
@@ -0,0 +1,20 @@
+using NKMCore.Hex;
+using Xunit;
+
+namespace NKMCore.Tests
+{
+    public class HexDirectionTests
+    {
+        [Theory]
+        [InlineData(HexDirection.Ne, HexDirection.Nw, HexDirection.E)]
+        [InlineData(HexDirection.Nw, HexDirection.W, HexDirection.Ne)]
+        [InlineData(HexDirection.Se, HexDirection.E, HexDirection.Sw)]
+        public void NearbyDirectionsAreCorrect(HexDirection direction, HexDirection expectedDirection1, HexDirection expectedDirection2)
+        {
+            HexDirection[] directions = direction.NearbyDirections();
+            HexDirection[] expectedDirections = {expectedDirection1, expectedDirection2};
+
+            Assert.Equal(expectedDirections, directions);
+        }
+    }
+}
---
+++ NKMCore.Tests/HexMapSerializerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NKMCore.Tests
+{
+    public class HexMapSerializerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
