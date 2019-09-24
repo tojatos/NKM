6c8659baf29f028d2080568c6ec15ba7d15d87f8
Krzysztof Ruczkowski <krzysztof.ruczkowski@nokia.com>
2019-09-24 15:43:16 +0200

    Serializers - added tests for GamePreparerDependencies

--- NKMCore.Tests/SerializersTests.cs
+++ NKMCore.Tests/SerializersTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NKMCore.Extensions;
 using NKMCore.Templates;
 using Xunit;
@@ -23,6 +24,32 @@ namespace NKMCore.Tests
         public void PickTypeSerializeAndDeserialize(PickType type)
         {
             Assert.Equal(type, type.Serialize().DeserializePickType());
+        }
+        [Fact]
+        public void GamePreparerDependenciesSerializeAndDeserialize()
+        {
+          var deps = new GamePreparerDependencies
+          {
+              NumberOfPlayers = 2,
+              PlayerNames = new List<string> {
+                "Ryszard",
+                "Maciej",
+              },
+              NumberOfCharactersPerPlayer = 4,
+              BansEnabled = true,
+              NumberOfBans = 2,
+              PickType = PickType.Draft,
+              GameType = GameType.Local,
+          };
+          GamePreparerDependencies newDeps = deps.Serialize().DeserializeGamePreparerDependencies();
+          Assert.Equal(deps.NumberOfPlayers, newDeps.NumberOfPlayers);
+          Assert.Equal(deps.PlayerNames, newDeps.PlayerNames);
+          Assert.Equal(deps.NumberOfCharactersPerPlayer, newDeps.NumberOfCharactersPerPlayer);
+          Assert.Equal(deps.BansEnabled, newDeps.BansEnabled);
+          Assert.Equal(deps.NumberOfBans, newDeps.NumberOfBans);
+          Assert.Equal(deps.HexMap, newDeps.HexMap);
+          Assert.Equal(deps.PickType, newDeps.PickType);
+          Assert.Equal(deps.GameType, newDeps.GameType);
         }
     }
 }
